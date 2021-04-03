de9348240801e028a6d46c2b6f5533f832a2fb10
Marina PEREZ <akilaroha@hotmail.fr>
2021-04-03 16:20:36 +0200

    modif soutenance

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,4 +1,5 @@
 const helmet = require('helmet');
+//configure les entêtes http liées à la sécurité de xss
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -7,6 +8,9 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const xss = require('xss');
+//Le cross-site scripting est un type de faille de sécurité des sites web permettant d’injecter du contenu 
+//dans une page, provoquant ainsi des actions sur les navigateurs web visitant la page.
+//ces failles ménent à rediriger vers un autre site pour de l’hameçonnage ou encore de voler la session en récupérant les cookies.
 const html = xss('<script>alert("xss");</script>');
 console.log(html);
 //const middleware = require('./middleware/auth');
@@ -29,10 +33,13 @@ mongoose.connect(
   app.use(helmet());
   //app.use(xss()); requires a middleware function
   app.set('trust proxy', 1) // trust first proxy
+  //les session peuvent être utilisées pour s’identifier auprès du serveur et cibler ses attaques.
+  //pour contrer celà on utilise express-session
   app.use( session({
     secret : 's3Cur3',
     name : 'sessionId',
     })
+    // nom de cookie génériques
   );
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
