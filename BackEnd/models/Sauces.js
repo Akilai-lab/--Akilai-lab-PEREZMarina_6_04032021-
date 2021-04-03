016a613d3032a8db0f31a25ad12a60fed192747c
Marina PEREZ <akilaroha@hotmail.fr>
2021-04-03 16:22:50 +0200

    modif soutenance

--- BackEnd/models/Sauces.js
+++ BackEnd/models/Sauces.js
@@ -16,6 +16,8 @@ const sauceSchema = mongoose.Schema({
     usersDisliked: {type : Array, required : true}  //on y  ajoute le nom de l'user qui n'a pas aimé
     //donc si like == 1 on va update la sauce en donnant comme réponse usersLiked = userId
 });
+//on crée un schema qui va contenir les données d'une sauce
 
 sauceSchema.plugin(uniqueValidator);
+// ajoute une validation de pré-sauvegarde pour les champs uniques dans le schéma.
 module.exports = mongoose.model('Sauce', sauceSchema);
